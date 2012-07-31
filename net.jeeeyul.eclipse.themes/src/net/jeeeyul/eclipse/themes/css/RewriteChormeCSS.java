a0e4a06910a0ce8b0b638ed6fcf7bc1f581182fd
Jeeeyul <jeeeyul@gmail.com>
2012-07-31 11:42:52 +0900

    Signed-off-by: Jeeeyul <jeeeyul@gmail.com>

--- net.jeeeyul.eclipse.themes/src/net/jeeeyul/eclipse/themes/css/RewriteChormeCSS.java
+++ net.jeeeyul.eclipse.themes/src/net/jeeeyul/eclipse/themes/css/RewriteChormeCSS.java
@@ -1,22 +1,14 @@
 package net.jeeeyul.eclipse.themes.css;
 
-import java.io.InputStream;
 import java.io.StringReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.jeeeyul.eclipse.themes.ChromeThemeCore;
-
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.ui.css.core.dom.ExtendedCSSRule;
 import org.eclipse.e4.ui.css.core.dom.ExtendedDocumentCSS;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.dom.WidgetElement;
 import org.eclipse.swt.widgets.Display;
-import org.osgi.framework.Bundle;
 import org.w3c.css.sac.SelectorList;
 import org.w3c.dom.css.CSSRuleList;
 import org.w3c.dom.css.CSSStyleSheet;
