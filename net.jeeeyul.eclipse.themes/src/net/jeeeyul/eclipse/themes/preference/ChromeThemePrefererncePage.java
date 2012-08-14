bd99c9ec3ce2d66112ab304dd9636b3225bd529e
Jeeeyul <jeeeyul@gmail.com>
2012-08-15 00:17:32 +0900

    Signed-off-by: Jeeeyul <jeeeyul@gmail.com>

--- net.jeeeyul.eclipse.themes/src/net/jeeeyul/eclipse/themes/preference/ChromeThemePrefererncePage.java
+++ net.jeeeyul.eclipse.themes/src/net/jeeeyul/eclipse/themes/preference/ChromeThemePrefererncePage.java
@@ -240,7 +240,6 @@ public class ChromeThemePrefererncePage extends PreferencePage implements IWorkb
 		addPage(new PartPage("Inactive", false));
 		addPage(new CommonPartPage());
 		addPage(new ToolbarPage());
-		addPage(new SectionPage());
 		addPage(new ETCPage());
 	}
 }
