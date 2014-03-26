9579b4c9949a0a0602fda903e0556ea65c7ed15e
Jeeeyul <Jeeeyul@gmail.com>
2014-03-26 09:42:07 +0900

    Download icon was added

--- net.jeeeyul.eclipse.themes/src/net/jeeeyul/eclipse/themes/internal/linux/LaunchGTKWizardAction.java
+++ net.jeeeyul.eclipse.themes/src/net/jeeeyul/eclipse/themes/internal/linux/LaunchGTKWizardAction.java
@@ -1,11 +1,14 @@
 package net.jeeeyul.eclipse.themes.internal.linux;
 
+import net.jeeeyul.eclipse.themes.SharedImages;
+
 import org.eclipse.jface.action.Action;
 
 public class LaunchGTKWizardAction extends Action {
 
 	public LaunchGTKWizardAction() {
 		setText("Launch GTK2.0 RC Wizard");
+		setImageDescriptor(SharedImages.getImageDescriptor(SharedImages.DOWNLOAD));
 	}
 
 	@Override
