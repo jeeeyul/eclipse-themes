35d450dde1d5474092b3d6447ac100710d46e6b3
Jeeeyul Lee <jeeeyul@gmail.com>
2016-03-28 10:40:12 +0900

    무료 서버로 이전

--- net.jeeeyul.eclipse.themes.store/deploy.sh
+++ net.jeeeyul.eclipse.themes.store/deploy.sh
@@ -1,1 +1,10 @@
-DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy themes.jeeeyul.net --settings settings.json
+meteor build tarball
+mv tarball/net.jeeeyul.eclipse.themes.store.tar.gz ~/Workspaces/jeeeyul\@openshift.redhat.com/themes/
+cd ~/Workspaces/jeeeyul\@openshift.redhat.com/themes/
+rm -rf programs server
+rm -f main.js
+tar -xvf net.jeeeyul.eclipse.themes.store.tar.gz -s '/^bundle//'
+rm net.jeeeyul.eclipse.themes.store.tar.gz
+git add --all
+git commit -m "new publish"
+git push
